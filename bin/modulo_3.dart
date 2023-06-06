53543d39a652a7d7cc08b33dbe266a1c2224342f
brunolbahiense <brunoluisbahiense@gmail.com>
2023-06-06 14:28:27 -0300

    update estrutura dos arquivos

--- bin/modulo_3.dart
+++ bin/modulo_3.dart
@@ -2,7 +2,34 @@ import 'package:modulo_3/modulo_3.dart' as modulo_3;
 
 void main() {
   escolherMeioDeTransporte(Transporte.skate);
+  Set<String> registrosVisitados = <String>{};
+  registrosVisitados = registrarDestinos('Salvador', registrosVisitados);
+
+  Map<String, dynamic> registrarPrecos= {}; 
+  registrarPrecos['Salvador'] = 71.33; 
+  registrarPrecos['Vitoria'] = 123123; 
+  registrarPrecos.remove('Salvador');
+  print(registrarPrecos);
+
+  dynamic meuNumero = 7.1;
+  meuNumero = 'uma coisa';
+  print(meuNumero); 
+  
+  Viagem viagemHoje = Viagem();
+  print(viagemHoje.dinheiro);
+  print(Viagem.voo);
+
 }
+
+class Viagem{
+  static String voo = 'ASV-032';
+  double dinheiro = 7.4;
+}
+Set<String> registrarDestinos(String destino,Set<String> banco){
+  banco.add(destino); 
+  return banco; 
+}
+
 
 void escolherMeioDeTransporte(Transporte locomocao){
   switch (locomocao){
